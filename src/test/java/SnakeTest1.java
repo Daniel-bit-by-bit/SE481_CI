25bf03515671452f8d55c5c52d48eae7617b98b1
Daniel-bit-by-bit <79226305+Daniel-bit-by-bit@users.noreply.github.com>
2022-12-05 20:08:06 -0800

    first commit

---
+++ src/test/java/SnakeTest1.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SnakeTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testIsGameOver() {
+		//automatically passes
+		assertEquals(true, true);
+		//fail("Not yet implemented");
+	}
+
+}
